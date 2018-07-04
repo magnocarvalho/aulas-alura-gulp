80b3875504110d26a588a938eeff819fd7849c9f
Magno Carvalho <magnocarv@hotmail.com>
2018-07-04 00:25:17 -0300

    imagens compactadas

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var imagemin  = require('gulp-imagemin')
-gulp.src('src/img');
- 
-gulp.dest('src/img');
+var gulp = require('gulp'), imagemin  = require('gulp-imagemin');
+gulp.task('build-img', function() {
+    gulp.src('src/img/**/*').pipe(imagemin()).pipe(gulp.dest('src/img'));
+
+})
